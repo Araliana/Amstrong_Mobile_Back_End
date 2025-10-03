0f17763061c448edd494adaa71c092035ad3bcfc
Howard Javier Leslie <howard@adaremit.co.id>
2025-10-03 16:38:26 +0700

    router/gabut

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/screen/menu/menu.dart';
+import 'package:flutter_application_1/route.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,9 +10,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      home: MenuPage(),
+      routerConfig: AppRoutes.router,
     );
   }
 }

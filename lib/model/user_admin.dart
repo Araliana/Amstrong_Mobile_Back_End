cbcc64b1d8cebd30c70e8db9954ca6b124a6798c
Howard Javier Leslie <howard@adaremit.co.id>
2025-10-09 16:59:42 +0700

    produktif sanget

---
+++ lib/model/user_admin.dart
@@ -0,0 +1,39 @@
+class UserAdmin {
+  final int id;
+  final String username;
+  final String img;
+  final int password;
+  final DateTime lastLogin;
+  final DateTime? createdAt;
+
+  UserAdmin({
+    required this.id,
+    required this.username,
+    required this.img,
+    required this.password,
+    required this.lastLogin,
+    required this.createdAt,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'username': username,
+      'img': img,
+      'password': password,
+      'last_login': lastLogin,
+      'created_at': createdAt,
+    };
+  }
+
+  factory UserAdmin.fromMap(Map<String, dynamic> map) {
+    return UserAdmin(
+      id: map['id'],
+      username: map['username'],
+      img: map['img'],
+      password: map['password'],
+      lastLogin: map['last_login'],
+      createdAt: map['created_at'],
+    );
+  }
+}
---
+++ lib/utils/index.dart
@@ -0,0 +1,15 @@
+T enumFromString<T extends Enum>(Iterable<T> values, String value) {
+  return values.firstWhere(
+    (e) => e.name == value,
+    orElse: () => throw ArgumentError('No enum value "$value" found in $T'),
+  );
+}
+
+extension FirstWhereOrNullExtension<E> on Iterable<E> {
+  E? firstWhereOrNull(bool Function(E element) test) {
+    for (var element in this) {
+      if (test(element)) return element;
+    }
+    return null;
+  }
+}

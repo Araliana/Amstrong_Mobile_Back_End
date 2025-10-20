0830da3f557b234123295eaeac0bce53ef7c8afc
EmpthyLL <javileh40@gmail.com>
2025-10-20 09:05:00 +0700

    sefv

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -1,9 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/db/db_helper.dart';
-import 'package:flutter_application_1/model/access.dart';
-import 'package:flutter_application_1/model/user_admin.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthProvider with ChangeNotifier {
@@ -36,27 +32,31 @@ class AuthProvider with ChangeNotifier {
     _setLoading(false);
   }
 
-  Future<void> login(String username, String password) async {
+  Future<bool> login(String username, String password) async {
     _setLoading(true);
 
-    // Contoh ambil dari DB (atau API)
     final res = await db.get(
-      userAdminTable,
+      adminTables,
       where: "username = ? AND password = ?",
       whereArgs: [username, password],
     );
 
     if (res.isNotEmpty) {
-      currUser = UserAdmin.fromMap(res[0]);
-
       final prefs = await SharedPreferences.getInstance();
-      await prefs.setString("curr_user", jsonEncode(currUser!.toMap()));
-
-      notifyListeners();
+      await prefs.setString("curr_user_id", res[0]["id"]);
+      _setLoading(false);
+      return true;
     } else {
-      // handle error login
+      _setLoading(false);
+      return false;
     }
+  }
 
+  Future<void> logout() async {
+    _setLoading(true);
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove("curr_user_id");
+    currUserId = null;
     _setLoading(false);
   }
 }
